7978002ac4c9a2aa664d85b63e8ee5a3ee8568c7
brandon6190 <brandon6190@outlook.com>
2018-12-03 22:42:01 -0500

    created basic html structure including: heading, paragraph, image, and download button

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,14 @@ class App extends Component {
     return (
       <div>
         <h1>Swell</h1>
+        <img src="#" alt="swell logo" />
+        <p>Lorem ipsum dolor amet vice literally bicycle rights cornhole chicharrones asymmetrical
+          tumblr kinfolk, organic deep v. Locavore aesthetic everyday carry raw denim lomo neutra
+          banjo. Adaptogen health goth readymade, sartorial biodiesel fanny pack cornhole banh mi
+          ethical typewriter activated charcoal echo park. Godard roof party chambray, truffaut
+          marfa organic narwhal intelligentsia. Enamel pin meh wayfarers salvia, tote bag bicycle
+          rights ennui hot chicken chia.</p>
+        <a href="../../somefile.txt" download="somefile.txt">Download!</a>
       </div>
     );
   }
